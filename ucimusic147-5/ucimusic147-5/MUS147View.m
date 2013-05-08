a23e0efef0bb48a5100999fa61a21138245c00fa
Greg <gsjeckell@gmail.com>
2013-05-07 20:54:46 -0700

    drawRect initial tests

--- ucimusic147-5/ucimusic147-5/MUS147View.m
+++ ucimusic147-5/ucimusic147-5/MUS147View.m
@@ -22,14 +22,73 @@ extern MUS147AQPlayer* aqp;
     return self;
 }
 
-/*
+
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect
 {
     // Drawing code
+    
+    // DRAWING RECTANGLE
+    
+    CGPoint center = CGPointMake(self.frame.size.width / 2.0, self.frame.size.height / 2.0);
+    
+    float rectangleWidth = 100.0;
+    
+    float rectangleHeight = 100.0;
+    
+    CGContextRef ctx = UIGraphicsGetCurrentContext();
+    
+    CGContextAddRect(ctx,CGRectMake(center.x - (0.5 * rectangleWidth), center.y - (0.5 * rectangleHeight), rectangleWidth, rectangleHeight));
+    
+    CGContextSetLineWidth(ctx, 10);
+    
+    CGContextSetStrokeColorWithColor(ctx, [[UIColor grayColor] CGColor]);
+    
+    CGContextStrokePath(ctx);
+    
+    CGContextSetFillColorWithColor(ctx, [[UIColor greenColor] CGColor]);
+    
+    CGContextAddRect(ctx,CGRectMake(center.x - (0.5 * rectangleWidth), center.y - (0.5 * rectangleHeight), rectangleWidth, rectangleHeight));
+    
+    CGContextFillPath(ctx);
+    
+    
+    // DRAWING CIRCLE
+    
+    // CGPoint center = CGPointMake(self.frame.size.width / 2.0, self.frame.size.height / 2.0);
+    
+    // CGContextRef ctx = UIGraphicsGetCurrentContext();
+    
+    //  CGContextBeginPath(ctx);
+    
+    //  CGContextSetLineWidth(ctx, 5);
+    
+    //  CGContextAddArc(ctx, center.x, center.y, 100.0, 0, 2*M_PI, 0);
+    
+    //  CGContextStrokePath(ctx);
+    
+    
+    // DRAWING LINES
+    
+    // CGContextRef ctx = UIGraphicsGetCurrentContext();
+    
+    // CGContextBeginPath(ctx);
+    
+    // CGContextMoveToPoint(ctx, 20.0, 20.0);
+    
+    // CGContextAddLineToPoint(ctx, 250.0, 100.0);
+    
+    // CGContextAddLineToPoint(ctx, 100.0f, 200.0f);
+    
+    // CGContextSetLineWidth(ctx, 5);
+    
+    
+    // CGContextClosePath(ctx);
+    
+    // CGContextStrokePath(ctx);
 }
-*/
+
 
 -(void)doTouches:(NSSet *)touches withEvent:(UIEvent *)event
 {
